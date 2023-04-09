012d1ee8ac7bd8f90fe84c29c3629bedefecc86a
DevLJL <leonamjlima88@outlook.com>
2023-04-09 08:14:37 -0300

    .

--- src/uQuotedStr.pas
+++ src/uQuotedStr.pas
@@ -8,15 +8,15 @@ uses
 type
   TDBDriver = (dbFB, dbMSSQL, dbMYSQL, dbSQLITE, dbORACLE, dbPG);
 
-  function Quo(const AValue: String): String; overload;
-  function Quo(const AValue: Integer): String; overload;
-  function Quo(const AValue: Int64): String; overload;
-  function Quo(const AValue: Double; const ADecimalPlaces: Integer = 2): String; overload;
-  function Quo(const AValue: Currency; const ADecimalPlaces: Integer = 2): String; overload;
-  function Quo(const AValue: TDate; const ADBName: TDBDriver): String; overload;
-  function Quo(const AValue: TDateTime; const ADBName: TDBDriver): String; overload;
-  function QuoNull(const AValue: Integer): String; overload;
-  function QuoNull(const AValue: Int64): String; overload;
+  function Q(const AValue: String): String; overload;
+  function Q(const AValue: Integer): String; overload;
+  function Q(const AValue: Int64): String; overload;
+  function Q(const AValue: Double; const ADecimalPlaces: Integer = 2): String; overload;
+  function Q(const AValue: Currency; const ADecimalPlaces: Integer = 2): String; overload;
+  function Q(const AValue: TDate; const ADBName: TDBDriver): String; overload;
+  function Q(const AValue: TDateTime; const ADBName: TDBDriver): String; overload;
+  function QN(const AValue: Integer): String; overload;
+  function QN(const AValue: Int64): String; overload;
   function DateToSQLFormat(const AValue: TDate; const ADBName: TDBDriver): string;
   function DateTimeToSQLFormat(const AValue: TDateTime; const ADBName: TDBDriver): string;
 
@@ -27,22 +27,22 @@ uses
   System.Dateutils,
   System.StrUtils;
 
-function Quo(const AValue: String): String;
+function Q(const AValue: String): String;
 begin
   Result := QuotedStr(AValue);
 end;
 
-function Quo(const AValue: Integer): String;
+function Q(const AValue: Integer): String;
 begin
   Result := QuotedStr(AValue.ToString);
 end;
 
-function Quo(const AValue: Int64): String;
+function Q(const AValue: Int64): String;
 begin
   Result := QuotedStr(AValue.ToString);
 end;
 
-function Quo(const AValue: Double; const ADecimalPlaces: Integer = 2): String;
+function Q(const AValue: Double; const ADecimalPlaces: Integer = 2): String;
 var
   LFormat: TFormatSettings;
 begin
@@ -50,7 +50,7 @@ begin
   Result := QuotedStr(Format('%.' + IntToStr(ADecimalPlaces) + 'f', [AValue], LFormat));
 end;
 
-function Quo(const AValue: Currency; const ADecimalPlaces: Integer = 2): String;
+function Q(const AValue: Currency; const ADecimalPlaces: Integer = 2): String;
 var
   LFormat: TFormatSettings;
 begin
@@ -58,24 +58,24 @@ begin
   Result := QuotedStr(Format('%.' + IntToStr(ADecimalPlaces) + 'f', [AValue], LFormat));
 end;
 
-function Quo(const AValue: TDate; const ADBName: TDBDriver): String;
+function Q(const AValue: TDate; const ADBName: TDBDriver): String;
 begin
   Result := QuotedStr(DateToSQLFormat(AValue, ADBName));
 end;
 
-function Quo(const AValue: TDateTime; const ADBName: TDBDriver): String;
+function Q(const AValue: TDateTime; const ADBName: TDBDriver): String;
 begin
   Result := QuotedStr(DateTimeToSQLFormat(AValue, ADBName));
 end;
 
-function QuoNull(const AValue: Integer): String;
+function QN(const AValue: Integer): String;
 begin
   Result := QuotedStr(AValue.ToString);
   if (AValue = 0) then
     Result := 'NULL';
 end;
 
-function QuoNull(const AValue: Int64): String;
+function QN(const AValue: Int64): String;
 begin
   Result := QuotedStr(AValue.ToString);
   if (AValue = 0) then
